3b14fbd1ee319d4bd00781c904e13186a00ebaf8
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2023-11-19 12:04:00 -0800

    testToday1

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -1,12 +1,22 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Finder {
 
 	public Object lastIndexOf(ArrayList<String> al, String apple) {
 		// TODO Auto-generated method stub
-		return null;
+		String a =apple.toLowerCase();
+		int q = -1;
+		for (int i = 0; i < al.size(); i++) {
+			String b = al.get(i).toLowerCase();
+			System.out.println(a+ ", " + b);
+			if (a.equals(b)) {
+				q=i;
+			}
+		}
+		return q;
 	}
 
 }
